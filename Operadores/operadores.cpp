d272f8ae586dcb7a644a7b17a8aaed6965db5e8a
ander <ander.r8@gmail.com>
2022-03-21 11:58:55 -0300

    operadores atualização

--- Operadores/operadores.cpp
+++ Operadores/operadores.cpp
@@ -17,10 +17,10 @@ int main()
     return 0;
 }
 */
-
+/*
 int main()
 {
-    // Operdores unários, exemplos: ++ -- += -= *=
+    // Operdores Unários, exemplos: ++ -- += -= *=
 
     int n = 10;
     //n++; // igual à n = n + 1
@@ -29,5 +29,35 @@ int main()
     n = (10 + 20) / 2; // () Operador de Precedência 
 
     cout << n;
+    return 0;
+}
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* Operadores Unários: inversão de operadores*/
+
+/*
+    int num = 10;
+    int num2 = -num; // imprimi -10, se o num fosse -10 imprimiria 10, inverteria o sinal
+    cout << num2;
+*/
+
+/*
+    int num = 10;
+    cout << num++; // imprimi 10, pelos sinais estar á frente do num
+*/
+
+/*
+    int num = 10;
+    cout << ++num; // imprimi 11, porque o encremento esta antes do num
+*/
+
+    int num = 10;
+    //int num2 = num++;
+    int num2 = ++num;
+
+    cout << num2;
+
     return 0;
 }
