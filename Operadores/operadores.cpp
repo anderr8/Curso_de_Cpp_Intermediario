c0fe0425e1741bb486afc368251f441af0497375
ander <ander.r8@gmail.com>
2022-03-21 12:24:48 -0300

    variável (char)

--- Operadores/operadores.cpp
+++ Operadores/operadores.cpp
@@ -54,7 +54,7 @@ int main(int argc, char const *argv[])
 */
 
     int num = 10;
-    //int num2 = num++;
+    int num2 = num++;
     int num2 = ++num;
 
     cout << num2;
---
+++ VariavelChar/variavel_char.cpp
@@ -0,0 +1,35 @@
+// Tabela ASCII, site: https://web.fe.up.pt/~ee96100/projecto/Tabela%20ascii.htm
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+/*
+    char c = 'a'; // caractres tem que ser colocado entre ('')aspas simples.
+    cout << c;
+*/
+
+/*
+    char c1 = 'a'; // Na tabela ASCII a letra minúscula vale 97 em decimal.
+    char c2 = 'b'; // Na tabela ASCII a letra minúscula vale 98 em decimal.
+    int soma = c1 + c2;
+    cout << soma;
+*/
+
+    //char c = '''; // Foi interpretado como um caractere de encerramento
+    char c = '\''; // A barra invertida para a esquerda determina que um caractere. 
+    cout << c;
+
+
+    return 0;
+}
+
+
+
+
+
+
